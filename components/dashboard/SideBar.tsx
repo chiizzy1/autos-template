87ed27a9e9856728ae5f6664b21375f81f0d5b69
Chinwuba Israel <chiizzy5@gmail.com>
2023-07-29 22:28:23 +0100

    change icon

--- components/dashboard/SideBar.tsx
+++ components/dashboard/SideBar.tsx
@@ -1,11 +1,11 @@
 import { FC } from "react";
 import Link from "next/link";
-import { RxSketchLogo, RxDashboard, RxPerson } from "react-icons/rx";
 import { GiAutoRepair } from "react-icons/gi";
 import { FiUpload } from "react-icons/fi";
 import SignOutButton from "../ui/SignOutButton";
 import { AiOutlineCar } from "react-icons/ai";
 import { BsCalendarEvent } from "react-icons/bs";
+import { Home, LayoutGrid, User } from "lucide-react";
 
 interface SideBarProps {
   children: React.ReactNode;
@@ -18,18 +18,18 @@ const SideBar: FC<SideBarProps> = ({ children }) => {
         <div className="flex flex-col items-center">
           <Link href="/">
             <div className="bg-purple-800 text-white p-3 rounded-lg inline-block">
-              <RxSketchLogo size={20} />
+              <Home size={20} strokeWidth={1.25} />
             </div>
           </Link>
           <span className="border-b-[1px] border-gray-200 w-full p-2"></span>
           <Link href="/dashboard">
             <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-2 p-3 rounded-lg inline-block">
-              <RxDashboard size={20} />
+              <LayoutGrid size={20} strokeWidth={1.25} />
             </div>
           </Link>
           <Link href="/dashboard/customers">
             <div className="bg-gray-100 hover:bg-gray-200 cursor-pointer my-2 p-3 rounded-lg inline-block">
-              <RxPerson size={20} />
+              <User size={20} strokeWidth={1.25} />
             </div>
           </Link>
           <Link href="/dashboard/cars">
