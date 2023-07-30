8ddbeb63be0de8663b05d1ef9ee6dbe5913173d7
Chinwuba Israel <chiizzy5@gmail.com>
2023-07-30 14:31:54 +0100

    minor change

--- components/dashboard/RecentPayments.tsx
+++ components/dashboard/RecentPayments.tsx
@@ -12,29 +12,25 @@ const RecentPayments: FC<RecentPaymentsProps> = ({ repairs }) => {
     <div className="w-full scrollbar-thin scrollbar-thumb-gray-900 scrollbar-track-gray-100  col-span-1 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white overflow-y-scroll">
       <h1>Recent Payments</h1>
       <ul>
-        {repairs.map((obj: any) =>
-          obj.paid ? (
-            <li
-              key={obj.id}
-              className="bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 flex items-center cursor-pointer"
-            >
-              <div className="bg-purple-100 rounded-lg p-3">
-                <BiDollar className="text-purple-800" />
-              </div>
-              <div className="pl-4">
-                <p className="text-gray-800 font-bold">
-                  ${obj.estimatedCost.toLocaleString("en")}
-                </p>
-                <p className="text-gray-400 text-sm">{`${obj.owner.firstName} ${obj.owner.lastName}`}</p>
-              </div>
-              <p className="lg:flex md:hidden absolute right-6 text-sm">
-                {formatTimeToNow(new Date(obj.startDate))}
+        {repairs.map((obj: any) => (
+          <li
+            key={obj.id}
+            className="bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 flex items-center cursor-pointer"
+          >
+            <div className="bg-purple-100 rounded-lg p-3">
+              <BiDollar className="text-purple-800" />
+            </div>
+            <div className="pl-4">
+              <p className="text-gray-800 font-bold">
+                ${obj.estimatedCost.toLocaleString("en")}
               </p>
-            </li>
-          ) : (
-            ""
-          )
-        )}
+              <p className="text-gray-400 text-sm">{`${obj.owner.firstName} ${obj.owner.lastName}`}</p>
+            </div>
+            <p className="lg:flex md:hidden absolute right-6 text-sm">
+              {formatTimeToNow(new Date(obj.startDate))}
+            </p>
+          </li>
+        ))}
       </ul>
     </div>
   );
